2c00820c6f6ec967d9ec5b63b1d1ce524dcc3d3d
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2026-01-05 22:15:08 +0000

    Register `extract_hidden_states` as a speculator type
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

--- src/vllm_hidden_states_extractor/__init__.py
+++ src/vllm_hidden_states_extractor/__init__.py
@@ -1,7 +1,22 @@
-
 def register():
     from vllm import ModelRegistry
+    from vllm.transformers_utils.configs.speculators.algos import (
+        register_speculator,
+        update_eagle3,
+    )
 
+    @register_speculator("extract_hidden_states")
+    def update_extract_hidden_states(config_dict: dict, vllm_config: dict) -> None:
+        """
+        This is a fake speculator that extracts hidden states from the model. It pretends to be an eagle3 speculator.
+        """
+        update_eagle3(config_dict, vllm_config)
+        vllm_config["method"] = "eagle3"
+        vllm_config["architectures"] = ["HiddenStatesExtractor"]
+
+    print("HiddenStatesExtractor registered")
     if "HiddenStatesExtractor" not in ModelRegistry.get_supported_archs():
-        ModelRegistry.register_model("HiddenStatesExtractor", "vllm_hidden_states_extractor.model:HiddenStatesExtractor")
-
+        ModelRegistry.register_model(
+            "HiddenStatesExtractor",
+            "vllm_hidden_states_extractor.model:HiddenStatesExtractor",
+        )
