c6d7107a75080355c851a310cbb8d579019c1926
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2026-01-08 15:35:18 +0000

    Remove unneeded LlamaModel and replace with minimal DummyModel
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

--- src/vllm_hidden_states_extractor/model.py
+++ src/vllm_hidden_states_extractor/model.py
@@ -7,159 +7,50 @@ import torch
 import torch.nn as nn
 
 from vllm.attention.backends.abstract import AttentionBackend, AttentionType
-from vllm.compilation.decorators import support_torch_compile
 from vllm.config import VllmConfig, get_current_vllm_config
 from vllm.attention.selector import get_attn_backend
 from vllm.logger import init_logger
 from vllm.model_executor.layers.attention_layer_base import AttentionLayerBase
-from vllm.model_executor.layers.layernorm import RMSNorm
-from vllm.model_executor.layers.linear import ReplicatedLinear
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
 from vllm.distributed import get_tensor_model_parallel_world_size
 from vllm.model_executor.layers.vocab_parallel_embedding import (
     ParallelLMHead,
     VocabParallelEmbedding,
 )
-from vllm.model_executor.model_loader.weight_utils import (
-    default_weight_loader,
-    maybe_remap_kv_scale_name,
-)
 from vllm.model_executor.models.llama_eagle3 import Eagle3LlamaForCausalLM
 from vllm.multimodal.inputs import NestedTensors
 
-from vllm.model_executor.models.utils import (
-    get_draft_quant_config,
-    maybe_prefix,
-)
+from vllm.model_executor.models.utils import maybe_prefix
 from vllm.utils.torch_utils import kv_cache_dtype_str_to_dtype
 from vllm.v1.kv_cache_interface import FullAttentionSpec, KVCacheSpec
 
 logger = init_logger(__name__)
 
 
-@support_torch_compile(
-    dynamic_arg_dims={
-        "input_ids": 0,
-        "positions": -1,
-        "hidden_states": 0,
-        "input_embeds": 0,
-    }
-)
-class LlamaModel(nn.Module):
-    def __init__(
-        self,
-        *,
-        vllm_config: VllmConfig,
-        start_layer_id: int = 0,
-        prefix: str = "",
-        num_hidden_states: int = 3,
-    ) -> None:
+# @support_torch_compile(
+#     dynamic_arg_dims={
+#         "input_ids": 0,
+#         "positions": -1,
+#         "hidden_states": 0,
+#         "input_embeds": 0,
+#     }
+# )
+
+
+class DummyModel(nn.Module):
+    def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
+
         self.config = vllm_config.speculative_config.draft_model_config.hf_config
-        self.vocab_size = self.config.vocab_size
-
-        # Get drafter's quantization config
-        self.quant_config = get_draft_quant_config(vllm_config)
-
-        eagle_config = getattr(self.config, "eagle_config", None)
-        if eagle_config is not None and "use_aux_hidden_state" in eagle_config:
-            self.use_aux_hidden_state = eagle_config["use_aux_hidden_state"]
-        else:
-            self.use_aux_hidden_state = True
-
-        current_vllm_config = get_current_vllm_config()
+        self.num_hidden_states = len(
+            getattr(self.config, "eagle_aux_hidden_state_layer_ids", [])
+        )
 
         self.embed_tokens = VocabParallelEmbedding(
             self.config.vocab_size,
-            self.config.hidden_size // num_hidden_states,
+            self.config.hidden_size // self.num_hidden_states,
             prefix=maybe_prefix(prefix, "embed_tokens"),
         )
-
-        if self.use_aux_hidden_state:
-            if hasattr(self.config, "target_hidden_size"):
-                fc_input_size = self.config.target_hidden_size * num_hidden_states
-            else:
-                fc_input_size = (
-                    self.config.hidden_size * num_hidden_states // num_hidden_states
-                )
-            self.fc = ReplicatedLinear(
-                input_size=fc_input_size,
-                output_size=self.config.hidden_size // num_hidden_states,
-                bias=False,
-                params_dtype=vllm_config.model_config.dtype,
-                quant_config=self.quant_config,
-                prefix=maybe_prefix(prefix, "fc"),
-                return_bias=False,
-            )
-        self.norm = RMSNorm(
-            self.config.hidden_size // num_hidden_states,
-            eps=self.config.rms_norm_eps,
-        )
-
-    def embed_input_ids(self, input_ids: torch.Tensor) -> torch.Tensor:
-        return self.embed_tokens(input_ids)
-
-    def forward(
-        self,
-        input_ids: torch.Tensor,
-        positions: torch.Tensor,
-        hidden_states: torch.Tensor,
-        input_embeds: torch.Tensor | None = None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        if input_embeds is None:
-            input_embeds = self.embed_input_ids(input_ids)
-        assert hidden_states.shape[-1] == input_embeds.shape[-1]
-
-        residual = None
-        hidden_states, hidden_prenorm = self.norm(hidden_states, residual)
-        return hidden_states, hidden_prenorm
-
-    def load_weights(self, weights: Iterable[tuple[str, torch.Tensor]]) -> set[str]:
-        stacked_params_mapping = [
-            # (param_name, shard_name, shard_id)
-            (".qkv_proj", ".q_proj", "q"),
-            (".qkv_proj", ".k_proj", "k"),
-            (".qkv_proj", ".v_proj", "v"),
-            (".gate_up_proj", ".gate_proj", 0),
-            (".gate_up_proj", ".up_proj", 1),
-        ]
-        params_dict = dict(self.named_parameters())
-        loaded_params: set[str] = set()
-        for name, loaded_weight in weights:
-            if "midlayer." in name:
-                name = name.replace("midlayer.", "layers.0.")
-            # Handle kv cache quantization scales
-            if self.quant_config is not None and (
-                scale_name := self.quant_config.get_cache_scale(name)
-            ):
-                # Loading kv cache quantization scales
-                param = params_dict[scale_name]
-                weight_loader = getattr(param, "weight_loader", default_weight_loader)
-                loaded_weight = (
-                    loaded_weight if loaded_weight.dim() == 0 else loaded_weight[0]
-                )
-                weight_loader(param, loaded_weight)
-                loaded_params.add(scale_name)
-                continue
-            # Remapping the name FP8 kv-scale
-            if "scale" in name:
-                name = maybe_remap_kv_scale_name(name, params_dict)
-                if name is None:
-                    continue
-            for param_name, weight_name, shard_id in stacked_params_mapping:
-                if weight_name not in name:
-                    continue
-                name = name.replace(weight_name, param_name)
-                param = params_dict[name]
-                weight_loader = param.weight_loader
-                weight_loader(param, loaded_weight, shard_id)
-                break
-            else:
-                param = params_dict[name]
-                weight_loader = getattr(param, "weight_loader", default_weight_loader)
-                weight_loader(param, loaded_weight)
-            loaded_params.add(name)
-        return loaded_params
 
 
 class CacheOnlyAttentionLayer(nn.Module, AttentionLayerBase):
@@ -237,12 +128,6 @@ class HiddenStatesExtractor(Eagle3LlamaForCausalLM):
         # Store target layer count in draft config for
         # proper layer_types indexing in draft models
         self.config.target_layer_count = target_layer_num
-        self.model = LlamaModel(
-            vllm_config=vllm_config,
-            prefix="model",
-            start_layer_id=target_layer_num,
-            num_hidden_states=self.num_hidden_states,
-        )
 
         logit_scale = getattr(self.config, "logit_scale", 1.0)
         self.lm_head = ParallelLMHead(
@@ -272,6 +157,9 @@ class HiddenStatesExtractor(Eagle3LlamaForCausalLM):
                 for layer_idx in range(self.config.num_hidden_layers)
             ]
         )
+        self.model = DummyModel(
+            vllm_config=vllm_config, prefix=maybe_prefix(prefix, "model")
+        )
 
     def embed_input_ids(
         self,
@@ -279,7 +167,7 @@ class HiddenStatesExtractor(Eagle3LlamaForCausalLM):
         multimodal_embeddings: NestedTensors | None = None,
         is_multimodal: torch.Tensor | None = None,
     ) -> torch.Tensor:
-        return self.model.embed_input_ids(input_ids)
+        return self.model.embed_tokens(input_ids)
 
     def forward(
         self,
